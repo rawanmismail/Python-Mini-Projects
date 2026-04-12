90f3718aff570e1c2a5d546373b96bd3fd610f3d
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-04-12 12:56:27 +0400

    Update fortune-cookie.py

--- fortune-cookie.py
+++ fortune-cookie.py
@@ -6,6 +6,7 @@ fortune_number = random.randint(1,3)
 
 fortune_text = ''
 
+    fortune_text = 'You will have a great day!'
 if fortune_number == 1:
     fortune_text = 'Today will be tought.... but worth it.'
 
