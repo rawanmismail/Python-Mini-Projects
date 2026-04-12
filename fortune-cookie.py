4efd218882291d978e25d23f02a714a8678981ae
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-04-12 12:56:33 +0400

    Update fortune-cookie.py

--- fortune-cookie.py
+++ fortune-cookie.py
@@ -10,6 +10,7 @@ if fortune_number == 1:
     fortune_text = 'You will have a great day!'
 if fortune_number == 1:
     fortune_text = 'Today will be tought.... but worth it.'
+if fortune_number == 1:
 
 
 print (f'{fortune_text} Your lucky number is: {lucky_number}')
