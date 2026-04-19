c133ffd8fb7d92ca37d85bece0259ba23b4b86ae
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-04-19 15:19:58 +0400

    Create names-display.py

---
+++ names-display.py
@@ -0,0 +1,15 @@
+
+
+
+
+names = [] #empty list to store data
+
+for i in range(3): #repeat 3 times
+    name = input ('Enter a name: ')
+    names.append(name)
+
+for name in names:
+    print(name)
+
+
+
