037ed865c12b4c0cbebd3f0860975983c3451c3e
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-04-22 15:20:06 +0400

    Update python.py

--- python.py
+++ python.py
@@ -1,5 +1,6 @@
 age = 20
 name = 'Joseph'
 city = 'New York'
+house = '51 New Avenue, near Starbucks'
 
 print(f"My name is {name} and I am {age} years old. I live and work in {city}.")print(f"My name is {name} and I am {age} years old. I live and work in {city}. My house is located in {house}.")
