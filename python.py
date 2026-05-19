3c0df220878d9ef0dd1e84427299307786418509
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-05-19 13:53:33 +0400

    Update python.py

--- python.py
+++ python.py
@@ -1,5 +1,18 @@
-my_set = {1, 2, 3}
-my_set.add(4)
-my_set.remove(2)
-print(my_set) 
+def student():
+    print("Emily Rhodes")
+def major():
+    print("Computer Science")
+def university():
+    print("Harvard University") 
+def age():
+    print(21)
+def POB():
+    print("Boston, Massachusetts")  
 
+
+print("Student Data Log")
+student()
+major()
+university()
+age()
+POB()
