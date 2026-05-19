bd67342f79f38c6042b65e401d32253f16aacc9d
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-05-19 16:08:04 +0400

    Update python.py

--- python.py
+++ python.py
@@ -2,7 +2,6 @@ print("Online Data Logger")
 
 def major():
     print("Harvard University") 
-    print("Boston, Massachusetts")  
     print("Education and Teaching")
     print("Sports and Recreation")
 
