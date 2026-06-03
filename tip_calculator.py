41f023aa09288c18f3ef27bd659b5b8224a6ec13
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-06-03 19:13:10 +0400

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -2,7 +2,6 @@ def main():
     dollars = dollars_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to tip? "))
     tip = dollars * percent
-    print(f"Leave ${tip:.2f}")
     print(f"Leave ${tip:.2f} as a tip.")
 
 
