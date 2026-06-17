a5db719e55e842078ab0d8f372f533b902fd4213
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-06-17 19:17:43 +0400

    Update spellingbee.py

--- spellingbee.py
+++ spellingbee.py
@@ -1,6 +1,7 @@
 
-WORDS = {"PAIR": 4, "HAIR": 4, "CHAIR": 5}
+Words = {"PAIR": 4, "HAIR": 4, "CHAIR": 5}
 
 def main():
     print("Welcome to the Spelling Bee Game!")
     print("Your letters are: A  I  P  C  R  H  G")
+
