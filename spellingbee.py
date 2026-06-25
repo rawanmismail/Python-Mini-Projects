210c21571c89e9b8de678e02bf4f1224c4c2ff3d
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-06-25 16:25:44 +0400

    Update spellingbee.py

--- spellingbee.py
+++ spellingbee.py
@@ -8,6 +8,7 @@ def main():
     while len(Words) > 0:
         guess = input("Enter a word (or type 'exit' to quit): ").upper()
         if guess == "EXIT":
+            print("Thanks for playing!")
         elif guess in Words:
 
 main()
