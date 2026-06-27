551a9cae95feeb96a2dbdaf42ea9af2764674202
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-06-27 20:56:17 +0400

    Update python.py

--- python.py
+++ python.py
@@ -1,8 +1,3 @@
-
-def main():
-    print("Welcome to the Spelling Bee Game!")
-    print("Your letters are: A  I  P  C  R  H  G")
-
     while len(Words) > 0:
         print(f"{len(Words)} left!")
         guess = input("Guess a word: ")
