40f981713d46ac3f604b0da2598513fe3b18fe6d
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-06-30 17:21:39 +0400

    Update race_results.py

--- race_results.py
+++ race_results.py
@@ -12,5 +12,6 @@ results.remove(["Bowser", "Donkey Kong Jr."]) # removes the list from the list
 results.extend(["Bowser", "Donkey Kong Jr."]) # adds elements from another list
 
 results.remove("Bowser") #removes an item from the list
+results.insert(1, "Bowser") #inserts an item at a specific index
 
 print(results) #prints the list
