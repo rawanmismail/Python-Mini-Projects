b66b03aadcc35228718c339484e762f5a91e7ca1
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-06-30 17:11:18 +0400

    Update race_results.py

--- race_results.py
+++ race_results.py
@@ -10,3 +10,4 @@ results.append("Toad")
 results.append(["Bowser", "Donkey Kong Jr."]) #adds a list to the list
 results.remove(["Bowser", "Donkey Kong Jr."]) # removes the list from the list
 results.extend(["Bowser", "Donkey Kong Jr."]) # adds elements from another list
+
