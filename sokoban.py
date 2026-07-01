88f25f3cf6388c1ac28f33888f50c107503bf928
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-07-01 16:24:31 +0400

    Update sokoban.py

--- sokoban.py
+++ sokoban.py
@@ -22,4 +22,3 @@ def main(): # Defines a function named main. The program’s logic is placed ins
 
         print(history) # Prints the current history list to show the user the actions they have entered so far
 
-main() # Calls the main function to start the programmain() #  Calls the main function to start the program
