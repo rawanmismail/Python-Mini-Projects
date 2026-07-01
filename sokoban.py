0828f23ebf141c3ff146c54a5d31d58d15bc75f2
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-07-01 16:05:52 +0400

    Update sokoban.py

--- sokoban.py
+++ sokoban.py
@@ -3,3 +3,4 @@ def main(): # Defines a function named main. The program’s logic is placed ins
     history = [] # Creates an empty list called history to store the actions the user enters
 
     while True:
+        action = input("Enter an action (up, down, left, right) or 'undo' to undo the last action: ") # Prompts the user to enter an action and stores it in the variable action
