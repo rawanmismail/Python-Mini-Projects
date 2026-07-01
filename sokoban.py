13e2769a19ebbd494e7d5b54d5a5df038b838989
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-07-01 16:05:44 +0400

    Update sokoban.py

--- sokoban.py
+++ sokoban.py
@@ -1,3 +1,4 @@
 
+def main(): # Defines a function named main. The program’s logic is placed inside it.
     history = [] # Creates an empty list called history to store the actions the user enters
 
