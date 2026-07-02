32f6ec9dd14dc2bbb720e685f605fabe09498532
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-07-02 14:23:26 +0400

    Update shows.py

--- shows.py
+++ shows.py
@@ -13,7 +13,7 @@ def main():
     cleaned_shows = []
     for show in SHOWS:
         cleaned_shows.append(show.strip().title()) #Strip removes whitespace from the beginning and end of the string, and title() capitalizes the first letter of each word.
-        print(" ".join(cleaned_shows)) #Join combines the list of cleaned shows into a single string, with each show separated by a space.
+        print(", ".join(cleaned_shows)) #Join combines the list of cleaned shows into a single string, with each show separated by a space.
 
     print(cleaned_shows)
 
