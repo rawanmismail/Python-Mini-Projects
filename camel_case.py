97d181e347d71f969af9ee5969b9bd066720a74b
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-07-08 17:59:58 +0400

    Update camel_case.py

--- camel_case.py
+++ camel_case.py
@@ -3,3 +3,8 @@
 2. Converting a string to camel case
 3. Checking if a letter is uppercase 
 
+The whole concept in simple terms:
+The user will enter variablename in camelCase
+My program will then convert it to snake_case
+"""
+
