b3fc4531ea592ad2e788d293b34b8b3ff7dbceef
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-07-10 13:54:47 +0400

    Update camel_case.py

--- camel_case.py
+++ camel_case.py
@@ -12,3 +12,4 @@ My program will then convert it to snake_case
 camel = input("camelCase:  ") #Get the input from the user
 for letter in camel: #Loop through each letter in the input string
     if letter.isupper(): #Check if the letter is uppercase
+        print("_" + letter.lower(), end = "") #If it is, print an underscore and the lowercase version of the letter
