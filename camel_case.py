e0c0a9c5d8520741b107224722a75fae0ee648b7
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-07-10 13:54:52 +0400

    Update camel_case.py

--- camel_case.py
+++ camel_case.py
@@ -14,3 +14,4 @@ for letter in camel: #Loop through each letter in the input string
     if letter.isupper(): #Check if the letter is uppercase
         print("_" + letter.lower(), end = "") #If it is, print an underscore and the lowercase version of the letter
     else:
+        print(letter, end = "") #If it isn't, print the letter as is
