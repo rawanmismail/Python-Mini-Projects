d75ce2828a2dd429400e967e95cad11befaaf4cb
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-07-20 17:07:19 +0400

    Update distances.py

--- distances.py
+++ distances.py
@@ -12,6 +12,5 @@ distances = {
 def main():
     spacecraft = input("Enter the name of the spacecraft: ")
     m = convert(distance[spacecraft])
-    print(f"The distance of {spacecraft} from Earth is {m} miles away.")
 
 main()
