68c49d4da1d70a3f62c2d65f0aafe14602595474
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-07-22 21:56:04 +0400

    Update distances.py

--- distances.py
+++ distances.py
@@ -14,7 +14,6 @@ def main():
     try:
         au = float(distances[spacecraft])
     except ValueError:
-        print(f"Can't convert {distances[spacecraft]} to a float.")
         return
     
     m = convert(au)
