74d4aec40fb069332f1cf6c4725b0570cff0b688
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-07-22 22:06:27 +0400

    Update boston_marathon.py

--- boston_marathon.py
+++ boston_marathon.py
@@ -3,3 +3,4 @@ def main():
     print(f"You need to run each mile in {round(pace,2)} minutes.")
     #The {round(pace,2)} = Rounds the value of pace to 2 decimal places.
 
+def get_pace(miles, minutes):
