e365f11bd2aeb046e65a5dea9de89ec45f26ebe2
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-08-04 17:33:05 +0400

    Update github_api.py

--- github_api.py
+++ github_api.py
@@ -8,3 +8,4 @@ response = requests.get(url)
 
 if response.status_code == 200: #MEANING: Did the server reply with 'Success'?
     user_data = response.json() #MEANING: Unpack the data into a Python dictionary
+    print(f"User: {user_data.get('login')}") #MEANING: Look up the key 'login' (the username) and print it
