bac9efea0f1263b922244764ce0f9bde42a1134d
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-08-04 17:42:29 +0400

    Update github_api.py

--- github_api.py
+++ github_api.py
@@ -11,4 +11,3 @@ if response.status_code == 200: #MEANING: Did the server reply with 'Success'?
     print(f"User: {user_data.get('login')}") #MEANING: Look up the key 'login' (the username) and print it
     print(f"Public Repos: {user_data.get('public_repos')}") #MEANING: Get the 'public_repos' value from the dictionary and print it
 else:
-    print("Failed to retrieve user data.")
