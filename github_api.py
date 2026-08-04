e0ac23ce916e9711b4992ecc9b3989b75d8479bd
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-08-04 17:33:08 +0400

    Update github_api.py

--- github_api.py
+++ github_api.py
@@ -9,3 +9,4 @@ response = requests.get(url)
 if response.status_code == 200: #MEANING: Did the server reply with 'Success'?
     user_data = response.json() #MEANING: Unpack the data into a Python dictionary
     print(f"User: {user_data.get('login')}") #MEANING: Look up the key 'login' (the username) and print it
+    print(f"Public Repos: {user_data.get('public_repos')}") #MEANING: Get the 'public_repos' value from the dictionary and print it
