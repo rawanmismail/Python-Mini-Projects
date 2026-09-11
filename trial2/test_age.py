655adb5a2536688b02e468c3b3308c117c5f062c
Rawan Mohamed <rawan.mohamedismail@yahoo.com>
2026-09-11 12:08:33 +0400

    Update test_age.py

--- trial2/test_age.py
+++ trial2/test_age.py
@@ -1,3 +1,8 @@
 from age import adult
 
 def test_adult():
+    assert adult(18) == True
+    assert adult(17) == False
+    assert adult(20) == True
+    assert adult(0) == False
+    assert adult(-1) == False
